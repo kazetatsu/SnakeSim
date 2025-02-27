d761293c506f803b1510a6cb6983ac6f187512ce
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-02-27 18:10:30 +0900

    Swing head with player's input

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Look : MonoBehaviour
+{
+    InputAction _actionPad;
+    InputAction _actionMouse;
+    HeadManager _manager;
+    float angLift = 0f;
+    [SerializeField] float angLiftLimit; 
+    [SerializeField] float angLiftSpeed;
+    float angYaw = 0f;
+    [SerializeField] float angYawLimit; 
+    [SerializeField] float angYawSpeed;
+
+    Vector2 currentInput;
+    [SerializeField] float sensitivityPad;
+    [SerializeField] float sensitivityMouse;
+
+    void Start() {
+        _actionPad   = InputSystem.actions.FindAction("LookPad");
+        _actionMouse = InputSystem.actions.FindAction("LookMouse");
+        _manager = this.transform.GetComponent<HeadManager>();
+    }
+
+    void Update() {
+        currentInput = sensitivityPad * _actionPad.ReadValue<Vector2>();
+        currentInput += sensitivityMouse * _actionMouse.ReadValue<Vector2>() / (float)Screen.width;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate() {
+        if (Mathf.Abs(currentInput.y) < 0.1f) {
+            if (Mathf.Abs(angLift) < 5f)
+                angLift = 0f;
+        } else {
+            angLift += currentInput.y * angLiftSpeed * Time.fixedDeltaTime;
+            if (angLift < -angLiftLimit) angLift = -angLiftLimit;
+            else if (angLift > angLiftLimit) angLift = angLiftLimit;
+        }
+
+        if (Mathf.Abs(currentInput.x) < 0.1f) {
+            if (Mathf.Abs(angYaw) < 5f)
+                angYaw = 0f;
+        } else {
+            angYaw += currentInput.x * angYawSpeed * Time.fixedDeltaTime;
+            if (angYaw > angYawLimit) angYaw = angYawLimit;
+            else if (angYaw < -angYawLimit) angYaw = -angYawLimit;
+        }
+
+        _manager.angLift = angLift;
+        _manager.angYaw  = angYaw;
+    }
+}
