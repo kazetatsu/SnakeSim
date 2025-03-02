62a865832ccc76bbd7cedc6a59fcb30902183768
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-03-02 13:50:42 +0900

    Adapt scripts to change of scene

--- Assets/Scripts/Vis.cs
+++ Assets/Scripts/Vis.cs
@@ -3,19 +3,17 @@ using UnityEngine;
 
 public class Vis : MonoBehaviour
 {
-    MiddleManager _manager;
+    JointsManager _manager;
     List<Material> _materials;
-    int jointCount;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _manager = GetComponent<MiddleManager>();
+        _manager = GetComponent<JointsManager>();
         _materials = new List<Material>();
-        jointCount = _manager.SegmentsCount;
 
-        for (int i = 0; i < jointCount; ++i) {
-            Transform child = this.transform.Find("Middle" + i.ToString("d2"));
+        for (int i = 1; i <= _manager.JointsCount; ++i) {
+            Transform child = this.transform.Find("Segment " + i.ToString("d2"));
 
             _materials.Add(
                 child
@@ -30,12 +28,10 @@ public class Vis : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < jointCount; ++i) {
-            float ang = _manager.jointAngles[i];
-            ang /=60f;
-            if (ang < 0f)  ang = -ang;
-            if (ang >= 1f) ang = 1f;
-            _materials[i].color = new Color(ang,0f,0f);
+        for (int i = 0; i < _manager.JointsCount; ++i) {
+            float ang = _manager.targetRotations[i].eulerAngles.x;
+            float r = Mathf.Min(ang, 360f-ang) / 60f;
+            _materials[i].color = new Color(r, 0f, 0f);
         }
     }
 }
