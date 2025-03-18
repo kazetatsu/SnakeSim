8ec51e16141a176608788747679d8ee19efaa4f8
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-03-18 17:34:23 +0900

    All graphic body chase physics body

--- Assets/Scripts/Skin.cs
+++ Assets/Scripts/Skin.cs
@@ -14,39 +14,30 @@ public class Skin : MonoBehaviour
     Transform[] backs;
 
     int vertsCount;
-    Vector3[][] localFVerts; // forward vertices
+    Vector3[] localVerts;
     int[][] localFVertIndexs;
-    Vector3[][] localBVerts; // backward vertices
     int[][] localBVertIndexs;
 
 
     class SlerpHelper {
-        float[] from;
         float[] to;
         float theta;
         bool tooClose = false;
 
-        public SlerpHelper(Quaternion from, Quaternion to) {
-            this.from = new float[4]{from.w, from.x, from.y, from.z};
-            this.to = new float[4]{to.w, to.x, to.y, to.z};
-
-            float cos = 0f;
-            for (int i = 0; i < 4; ++i) 
-                cos += this.from[i] * this.to[i];
-            if (cos < 0f) {
-                for (int i = 0; i < 4; ++i) 
-                    this.to[i] = - this.to[i];
-                cos = -cos;
+        public SlerpHelper(Quaternion to) {
+            if (to.w >= 0f) {
+                this.to = new float[4]{ to.w,  to.x,  to.y,  to.z};
+            } else {
+                this.to = new float[4]{-to.w, -to.x, -to.y, -to.z};
             }
 
-            theta = Mathf.Acos(cos);
+            theta = Mathf.Acos(to.w);
 
             if (theta <= 0.001f) {
                 tooClose = true;
             } else {
                 float invSin = 1f / Mathf.Sin(theta);
                 for (int i = 0; i < 4; ++i) {
-                    this.from[i] *= invSin;
                     this.to[i] *= invSin;
                 }
             }
@@ -54,20 +45,32 @@ public class Skin : MonoBehaviour
 
         public Quaternion Slerp(float t) {
             if (tooClose) {
-                return new Quaternion(from[1], from[2], from[3], from[0]);
+                return new Quaternion(to[1], to[2], to[3], to[0]);
             } else {
                 float coefF = Mathf.Sin((1f-t) * theta);
                 float coefT = Mathf.Sin(t * theta);
                 return new Quaternion(
-                    coefF * from[1] + coefT * to[1],
-                    coefF * from[2] + coefT * to[2],
-                    coefF * from[3] + coefT * to[3],
-                    coefF * from[0] + coefT * to[0]
+                    coefT * to[1],
+                    coefT * to[2],
+                    coefT * to[3],
+                    coefT * to[0] + coefF
                 );
             }
         }
     }
-
+    Quaternion GetHalfRotation (Quaternion rot) {
+        float[] q;
+        if (rot.w >= 0f) {
+            q = new float[4]{1f + rot.w,  rot.x,  rot.y,  rot.z};
+        } else {
+            q = new float[4]{1f - rot.w, -rot.x, -rot.y, -rot.z};
+        }
+        float norm = 0f;
+        for (int i = 0; i < 4; ++i)
+            norm += q[i] * q[i];
+        norm = Mathf.Sqrt(norm);
+        return new Quaternion(q[1]/norm, q[2]/norm, q[3]/norm, q[0]/norm);
+    }
 
     void Start() {
         segmentsCount = physicalBody.childCount;
@@ -76,14 +79,11 @@ public class Skin : MonoBehaviour
 
         Vector3[] verts = _mesh.vertices;
 
-        var localFVerts = new List<Vector3>[segmentsCount];
+        localVerts = new Vector3[verts.Length];
         var localFVertIndexs = new List<int>[segmentsCount];
-        var localBVerts = new List<Vector3>[segmentsCount];
         var localBVertIndexs = new List<int>[segmentsCount];
         for (int i = 0; i < segmentsCount; ++i) {
-            localFVerts[i] = new List<Vector3>();
             localFVertIndexs[i] = new List<int>();
-            localBVerts[i] = new List<Vector3>();
             localBVertIndexs[i] = new List<int>();
         }
 
@@ -94,25 +94,20 @@ public class Skin : MonoBehaviour
             if (i < 0) i = 0;
             if (i >= segmentsCount) i = segmentsCount - 1;
 
-            Vector3 localVert = verts[k] + ((float)i + 0.5f) * dist * Vector3.forward;
+            localVerts[k] = verts[k] + ((float)i + 0.5f) * dist * Vector3.forward;
+
             if (zd - (float)i <= 0.5f) {
-                localFVerts[i].Add(localVert);
                 localFVertIndexs[i].Add(k);
             } else {
-                localBVerts[i].Add(localVert);
                 localBVertIndexs[i].Add(k);
             }
         }
 
-        this.localFVerts = new Vector3[segmentsCount][];
         this.localFVertIndexs = new int[segmentsCount][];
-        this.localBVerts = new Vector3[segmentsCount][];
         this.localBVertIndexs = new int[segmentsCount][];
         for (int i = 0; i < segmentsCount; ++i) {
-            this.localFVerts[i] = localFVerts[i].ToArray();
             this.localFVertIndexs[i] = localFVertIndexs[i].ToArray();
-            this.localBVerts[i] = localFVerts[i].ToArray();
-            this.localBVertIndexs[i] = localFVertIndexs[i].ToArray();
+            this.localBVertIndexs[i] = localBVertIndexs[i].ToArray();
         }
 
         backs = new Transform[segmentsCount];
@@ -129,17 +124,79 @@ public class Skin : MonoBehaviour
 
 
     void Update() {
-        var verts = _mesh.vertices;
+        var verts = new Vector3[vertsCount];
 
-        int n = localFVerts[0].Length;
         Vector3 right = backs[0].right;
         Vector3 up = backs[0].up;
         Vector3 forward = backs[0].forward;
         Vector3 center = backs[0].position;
-        for (int s = 0; s < n; ++s) {
-            int k = localFVertIndexs[0][s];
-            Vector3 localVert = localFVerts[0][s];
+        float disth = 0.5f * dist;
+        float aspect = radius / disth;
+        float invRadius = 1f/radius;
+
+        foreach (int k in localFVertIndexs[0]) {
+            Vector3 localVert = localVerts[k];
             verts[k] = localVert.x * right + localVert.y * up + localVert.z * forward + center;
+        }
+
+        for (int i = 0; i < segmentsCount-1; ++i) {
+            var roth = GetHalfRotation(Quaternion.Inverse(backs[i].rotation) * backs[i+1].rotation);
+            var helper = new SlerpHelper(roth);
+
+            foreach (int k in localBVertIndexs[i]) {
+                float t = -localVerts[k].z / disth;
+                Quaternion rott = helper.Slerp(t);
+                right = rott * Vector3.right;
+                up = rott * Vector3.up;
+                Vector3 rotatedXY = localVerts[k].x * right + localVerts[k].y * up;
+                Vector3 direction = rotatedXY.normalized;
+                float xy2 = direction.x * direction.x + direction.y * direction.y;
+
+                Vector3 localVert;
+                if (-t + aspect * direction.z / Mathf.Sqrt(xy2) >= -1f) {
+                    localVert = rotatedXY * Mathf.Sqrt(1f + direction.z * direction.z / xy2);
+                } else {
+                    float zdh = localVerts[k].z + disth;
+                    float a = -direction.z * zdh + Mathf.Sqrt(-xy2 * zdh * zdh + radius * radius);
+                    localVert = a * invRadius * rotatedXY;
+                }
+                localVert.z += localVerts[k].z;
+
+                verts[k] = backs[i].rotation * localVert + backs[i].position;
+            }
+        }
+
+        for (int i = 1; i < segmentsCount; ++i) {
+            var roth = GetHalfRotation(Quaternion.Inverse(backs[i].rotation) * backs[i-1].rotation);
+            var helper = new SlerpHelper(roth);
+
+            foreach (int k in localFVertIndexs[i]) {
+                float t = localVerts[k].z / disth;
+                Quaternion rott = helper.Slerp(t);
+                right = rott * Vector3.right;
+                up = rott * Vector3.up;
+                Vector3 rotatedXY = localVerts[k].x * right + localVerts[k].y * up;
+                Vector3 direction = rotatedXY.normalized;
+                float xy2 = direction.x * direction.x + direction.y * direction.y;
+
+                Vector3 localVert;
+                if (t + aspect * direction.z / Mathf.Sqrt(xy2) <= 1f) {
+                    localVert = rotatedXY * Mathf.Sqrt(1f + direction.z * direction.z / xy2);
+                } else {
+                    float zdh = localVerts[k].z - disth;
+                    float a = -direction.z * zdh + Mathf.Sqrt(-xy2 * zdh * zdh + radius * radius);
+                    localVert = a * invRadius * rotatedXY;
+                }
+                localVert.z += localVerts[k].z;
+
+                verts[k] = backs[i].rotation * localVert + backs[i].position;
+            }
+        }
+
+        Quaternion rot = backs[segmentsCount-1].rotation;
+        center = backs[segmentsCount-1].position;
+        foreach (int k in localBVertIndexs[segmentsCount-1]) {
+            verts[k] = rot * localVerts[k] + center;
         }
 
         _mesh.SetVertices(verts);
