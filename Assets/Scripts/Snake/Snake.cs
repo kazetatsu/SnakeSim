2f4210f90d576fed7b3d7de9829be9f499ea492b
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-11-01 13:21:22 +0900

    Rename Constsntants->Snake & Create dummy head

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -1,6 +1,11 @@
-public static class Consts {
+using UnityEngine;
+
+public static class Snake
+{
     public const int JointsCount = 15;
     public const int SegmentsCount = 16;
     public const float SegmentsDist = 1.2f;
     public const float SegmentRadius = .5f;
+
+    public static Vector3 headPos = Vector3.zero;
 }
---
+++ Assets/Scripts/SnakeHeadDummy.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SnakeHeadDummy : MonoBehaviour
+{
+    InputAction action;
+
+    [SerializeField] float speed;
+
+    void Start() {
+        action = InputSystem.actions.FindAction("MoveDebug");
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, 1f);
+    }
+
+    void Update() {
+        var v = speed * action.ReadValue<Vector3>();
+        transform.position += v * Time.deltaTime;
+        Snake.headPos = transform.position;
+    }
+}
