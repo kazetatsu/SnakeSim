2f8044265a60763733db0959c2246254ae6afaa4
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-11-09 21:54:40 +0900

    Brow wind in Aki stage

---
+++ Assets/Scripts/Stage/Wind.cs
@@ -0,0 +1,49 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] Vector3 pushForce;
+
+    Rigidbody[] segmentBodys;
+    bool[] isSegmentInside;
+
+
+    void Start() {
+        isSegmentInside = new bool[Snake.SegmentsCount];
+        segmentBodys = new Rigidbody[Snake.SegmentsCount];
+
+        for (int i = 0; i < Snake.SegmentsCount; ++i)
+            isSegmentInside[i] = false;
+    }
+
+
+    void Update() {}
+
+
+    void OnTriggerEnter(Collider other) {
+        Transform segment = other.transform.parent.parent;
+        int i = segment.GetSiblingIndex(); // index of hit segment
+        if (segmentBodys[i] is null || segmentBodys[i].IsDestroyed())
+            segmentBodys[i] = segment.GetChild(0).GetComponent<Rigidbody>();
+        isSegmentInside[i] = true;
+    }
+
+
+    void OntriggerExit(Collider other) {
+        int i = other.transform.parent.parent.GetSiblingIndex(); // index of hit segment
+        isSegmentInside[i] = false;
+    }
+
+
+    void FixedUpdate() {
+        for (int i = 0; i < Snake.SegmentsCount; ++i) {
+            if (segmentBodys[i] is null || segmentBodys[i].IsDestroyed())
+                isSegmentInside[i] = false;
+
+            if (!isSegmentInside[i]) continue;
+
+            segmentBodys[i].AddForce(pushForce, ForceMode.Force);
+        }
+    }
+}
