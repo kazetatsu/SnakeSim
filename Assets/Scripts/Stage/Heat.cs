397ad3e528913709f0929ece3717e7240675b0e3
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-11-09 18:34:39 +0900

    Snake jump when touching natsu's floor too long

---
+++ Assets/Scripts/Stage/Heat.cs
@@ -0,0 +1,66 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Heat : MonoBehaviour {
+    [SerializeField] float jumpImpulse;
+    [SerializeField] float timeTilljump;
+
+    Rigidbody[] segmentBodys;
+
+    // Indexs of a collider s.t. each segment of the snake is touching the collider.
+    int[] touchIndexs;
+    // Time s.t. each segment of the snake is touching the collider.
+    float[] touchTimes;
+
+
+    public void OnSnakeTouch(int colliderIndex, Transform segment) {
+        int segmentIndex = segment.GetSiblingIndex();
+        if (segmentBodys[segmentIndex] is null || segmentBodys[segmentIndex].IsDestroyed())
+            segmentBodys[segmentIndex] = segment.GetChild(0).GetComponent<Rigidbody>();
+        touchIndexs[segmentIndex] = colliderIndex;
+    }
+
+
+    public void OnSnakeLeave(int colliderIndex, Transform segment) {
+        int segmentIndex = segment.GetSiblingIndex();
+        if (touchIndexs[segmentIndex] != colliderIndex)
+            return;
+        touchIndexs[segmentIndex] = -1;
+        touchTimes[segmentIndex] = 0f;
+    }
+
+
+    void Start() {
+        touchIndexs = new int[Snake.SegmentsCount];
+        touchTimes = new float[Snake.SegmentsCount];
+        segmentBodys = new Rigidbody[Snake.SegmentsCount];
+
+        for (int i = 0; i < Snake.SegmentsCount; ++i) {
+            touchIndexs[i] = -1;
+            touchTimes[i] = 0f;
+        }
+
+        for (int j = transform.childCount - 1; j >= 0; --j)
+            transform.GetChild(j).AddComponent<HotFloor>();
+    }
+
+
+    void Update() {}
+
+
+    void FixedUpdate() {
+        for (int i = 0; i < Snake.SegmentsCount; ++i) {
+            // If the snake respawn when touching hot floor
+            if (segmentBodys[i] is null || segmentBodys[i].IsDestroyed())
+                touchIndexs[i] = -1;
+
+            if (touchIndexs[i] < 0) continue;
+
+            touchTimes[i] += Time.fixedDeltaTime;
+            if (touchTimes[i] > timeTilljump) {
+                touchTimes[i] = 0f;
+                segmentBodys[i].AddForce(jumpImpulse * Vector3.up, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Stage/HotFloor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class HotFloor : MonoBehaviour {
+    int index;
+    Heat heat;
+
+
+    void Start() {
+        index = transform.GetSiblingIndex();
+        heat = transform.parent.GetComponent<Heat>();
+    }
+    void Update() { }
+
+
+    void OnCollisionEnter(Collision collision) {
+        heat.OnSnakeTouch(index, collision.transform.parent);
+    }
+
+    void OnCollisionExit(Collision collision) {
+        heat.OnSnakeLeave(index, collision.transform.parent);
+    }
+}
