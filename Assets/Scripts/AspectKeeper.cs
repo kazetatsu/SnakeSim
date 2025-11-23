2cd41b5fcefae012b5e46c0a49a3fbed43e6082a
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-11-23 19:05:00 +0900

    Keep aspect ratio

---
+++ Assets/Scripts/AspectKeeper.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class AspectKeeper : MonoBehaviour
+{
+    Camera _camera;
+
+
+    void Start() {
+        _camera = GetComponent<Camera>();
+    }
+
+
+    void Update() {
+        float w, h;
+        if (Screen.width > Screen.height) {
+            w = (float)Screen.height / (float)Screen.width;
+            h = 1f;
+        } else {
+            w = 1f;
+            h = (float)Screen.width / (float)Screen.height;
+        }
+
+        _camera.rect = new Rect(0.5f - w*0.5f, 0.5f - h*0.5f, w, h);
+    }
+}
