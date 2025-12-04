214a5474dcb469f919530db5409cb2937638204a
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-12-04 17:11:04 +0900

    Add forework

---
+++ Assets/Scripts/Ending/FireWork.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class FireWork : MonoBehaviour
+{
+    [SerializeField] Color[] colors;
+    [SerializeField] float delay;
+    [SerializeField] float interval;
+    float timer;
+
+    ParticleSystem particle;
+
+
+    void Start() {
+        particle = GetComponent<ParticleSystem>();
+        timer = delay;
+    }
+
+
+    void Update() {
+        if (timer < interval) {
+            timer += Time.deltaTime;
+        } else {
+            var temp  = particle.main;
+            temp.startColor = colors[Random.Range(0,colors.Length)];
+            particle.Play();
+            timer = 0f;
+        }
+    }
+}
