711963455a7fa458c2a578ef64be59a770b2b609
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-12-10 18:17:07 +0900

    Back to title from ending

---
+++ Assets/Scripts/Ending/Exit.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Exit : MonoBehaviour
+{
+    [SerializeField] float delay;
+    float timer = 0f;
+
+    InputAction action;
+
+
+    void Start() {
+        action = InputSystem.actions.FindAction("Spawn");
+    }
+
+
+    void Update() {
+        if (EventSystem.current is null) return;
+
+        if (timer < delay) {
+            timer += Time.unscaledDeltaTime;
+        } else if (action.IsPressed()) {
+            GameObject.Find("SceneLoader")?.GetComponent<SceneLoader>().ToTitle();
+            Destroy(this);
+        }
+    }
+}
