cb0851e8cce987a665d94c180ed57a0d80aec598
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2025-12-20 15:24:55 +0900

    Create key visual

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScreenShot : MonoBehaviour
+{
+    InputAction action;
+    int cnt = 0;
+
+
+    string pngPath(int i) {
+        return Path.Combine(Application.dataPath, "Capture", "Movie" + i.ToString() + ".png");
+    }
+
+    void Start() {
+        action = InputSystem.actions.FindActionMap("Debug").FindAction("Capture");
+        Screen.fullScreen = true;
+    }
+
+
+    void Update() {
+        if (action.IsPressed()) {
+            ScreenCapture.CaptureScreenshot(pngPath(0), 2);
+            ++cnt;
+        }
+    }
+}
